78d87ddb30c64d7c2a7b47656823a07f184b6f55
guqing <38999863+guqing@users.noreply.github.com>
2024-07-02 17:53:24 +0800

    chore: remove deprecated plugin wrapper (#46)
    ### What this PR does?
    移除对已过时的 PluginWrapper 的引用，Halo 2.18.0 版本后将不在支持从 BasePlugin 中获取 PluginWrapper ，也不再支持依赖注入 PluginWrapper，使用 PluginContext 代替
    
    see also https://github.com/halo-dev/halo/pull/6243 for more details
    
    ```release-note
    None
    ```

--- src/main/java/run/halo/photos/PhotoPlugin.java
+++ src/main/java/run/halo/photos/PhotoPlugin.java
@@ -1,9 +1,9 @@
 package run.halo.photos;
 
-import org.pf4j.PluginWrapper;
 import org.springframework.stereotype.Component;
 import run.halo.app.extension.SchemeManager;
 import run.halo.app.plugin.BasePlugin;
+import run.halo.app.plugin.PluginContext;
 
 /**
  * @author ryanwang
@@ -12,18 +12,18 @@ import run.halo.app.plugin.BasePlugin;
 @Component
 public class PhotoPlugin extends BasePlugin {
     private final SchemeManager schemeManager;
-    
-    public PhotoPlugin(PluginWrapper wrapper, SchemeManager schemeManager) {
-        super(wrapper);
+
+    public PhotoPlugin(PluginContext pluginContext, SchemeManager schemeManager) {
+        super(pluginContext);
         this.schemeManager = schemeManager;
     }
-    
+
     @Override
     public void start() {
         schemeManager.register(Photo.class);
         schemeManager.register(PhotoGroup.class);
     }
-    
+
     @Override
     public void stop() {
         schemeManager.unregister(schemeManager.get(Photo.class));
